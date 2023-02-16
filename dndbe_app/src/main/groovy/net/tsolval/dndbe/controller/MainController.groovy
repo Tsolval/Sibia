07f1b4b1153318c8bc581d0619d26ffb65eedcad
Tsolval <tsolval@gmail.com>
2023-02-15 20:19:20 -0600

    move template to home page

--- dndbe_app/src/main/groovy/net/tsolval/dndbe/controller/MainController.groovy
+++ dndbe_app/src/main/groovy/net/tsolval/dndbe/controller/MainController.groovy
@@ -1,15 +1,19 @@
 package net.tsolval.dndbe.controller
 
+import org.springframework.boot.Banner
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 
-@RestController
+@Controller
 @RequestMapping('/')
 class MainController {
 
     @GetMapping
     def showDashboard() {
         'Greetings from Spring Boot!'
+        new ModelAndView ('views/home', [bootVersion  : Banner.package.implementationVersion,
+                                         groovyVersion: GroovySystem.version])
     }
 }
